548d9563323ff303fc728e77017c2e837a06b902
Dennis Kimathi <kimathidennis38@gmail.com>
2022-06-09 11:07:39 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // code your solution here
 
+//Variable Declaration
 function saturdayFun(activity="roller-skate") {
     return `This Saturday, I want to ${activity}!`
 
@@ -7,8 +8,8 @@ function saturdayFun(activity="roller-skate") {
 
 saturdayFun("bathe my dog")
 
-
-function mondayWork(activityMonday="go to the office") {
+//Variable Expression
+const  mondayWork = function(activityMonday="go to the office") {
     return `This Monday, I will ${activityMonday}.`
 
 }
